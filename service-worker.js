28d3beb5d089ae5f0eec811f314e4820a23a0ab1
Chibron <ilanmsoilipro@gmail.com>
2026-06-10 22:27:20 +0200

    L'application ne fonctionne pas si l'utilsateur refuse et est hors ligne

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'coracia-v1';
+const CACHE = 'coracia-v2';
 
 const PRECACHE = [
   './',
@@ -41,12 +41,50 @@ const PRECACHE = [
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
 ];
 
+// Tuiles Maptiler pour la zone de Sillans-la-Cascade (offline map)
+const TILE_BASE = 'https://api.maptiler.com/maps/streets-v2';
+const TILE_KEY  = 'j5GTbvHC4ExI3Tj8orVK';
+const TILE_BBOX = { minLat: 43.558, maxLat: 43.572, minLng: 6.175, maxLng: 6.190 };
+const TILE_ZOOMS = [13, 14, 15, 16, 17];
+
+function latLngToTile(lat, lng, z) {
+  const n = Math.pow(2, z);
+  const x = Math.floor((lng + 180) / 360 * n);
+  const r = lat * Math.PI / 180;
+  const y = Math.floor((1 - Math.log(Math.tan(r) + 1 / Math.cos(r)) / Math.PI) / 2 * n);
+  return { x, y };
+}
+
+function generateTileUrls() {
+  const urls = [];
+  for (const z of TILE_ZOOMS) {
+    const { x: x0, y: y0 } = latLngToTile(TILE_BBOX.maxLat, TILE_BBOX.minLng, z);
+    const { x: x1, y: y1 } = latLngToTile(TILE_BBOX.minLat, TILE_BBOX.maxLng, z);
+    for (let x = x0; x <= x1; x++) {
+      for (let y = y0; y <= y1; y++) {
+        urls.push(`${TILE_BASE}/${z}/${x}/${y}.png?key=${TILE_KEY}`);
+      }
+    }
+  }
+  return urls;
+}
+
+const BC = new BroadcastChannel('coracia-sw');
+
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      Promise.allSettled(PRECACHE.map(url => cache.add(url).catch(() => {})))
-    ).then(() => self.skipWaiting())
-  );
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    const all   = [...PRECACHE, ...generateTileUrls()];
+    let done = 0;
+    await Promise.allSettled(all.map(async url => {
+      try { await cache.add(url); } catch {}
+      done++;
+      if (done % 5 === 0 || done === all.length) {
+        BC.postMessage({ type: 'CACHE_PROGRESS', value: Math.round(done / all.length * 100) });
+      }
+    }));
+    await self.skipWaiting();
+  })());
 });
 
 self.addEventListener('activate', event => {
@@ -98,5 +136,8 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
+  if (event.data?.type === 'CLEAR_CACHE') {
+    event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  }
 });
